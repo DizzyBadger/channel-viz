225ff17da19b660c8910e80e0bf75357d61d00ab
DizzyBadger <jk@lokna.no>
2016-02-18 22:40:45 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'nHCcSk38d7i0GsVhL33FRNNhWsdiUzOvNJVEYHmt1GRYrFiy', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [658482089], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'BadgerMonitor', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
